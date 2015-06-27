73c2e3643d6730d3fa856b658359ca38620832dd
phonxis <phonxis@gmail.com>
2015-06-27 15:26:40 +0300

    working project

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
-from app import app, db, lm, oid
+from app import app, db, lm
 from .forms import LoginForm, PostForm
 from .models import User, Post
 from config import POSTS_PER_PAGE
@@ -20,7 +20,6 @@ def before_request():
 @app.route('/index/<int:page>')
 def index(page=1):
 	user = g.user
-	posts = []
 	p = Post.query.order_by(Post.timestamp.desc()).paginate(page, POSTS_PER_PAGE, False)
 	return render_template('index.html',
 							title='Home',
@@ -44,26 +43,6 @@ def login():
 	return render_template('login.html', 
 							title='Sign In',
 							form=form)
-
-@oid.after_login
-def after_login(resp):
-	if resp.email is None or resp.email == "":
-		flash('Invalid login. Please try again.')
-		return redirect(url_for('login'))
-	user = User.query.filter_by(email=resp.email).first()
-	if user is None:
-		nickname = resp.nickname
-		if nickname is None or nickname == "":
-			nickname = resp.email.split('@')[0]
-		user = User(nickname=nickname, email=resp.email)
-		db.session.add(user)
-		db.session.commit()
-	remember_me = False
-	if 'remember_me' in session:
-		remember_me = session['remember_me']
-		session.pop('remember_me', None)
-	login_user(user, remember = remember_me)
-	return redirect(request.args.get('next') or url_for('index'))
 
 @app.route('/logout')
 def logout():
